2d6126941791692aec696bb659df3a725884a505
Scott Abbey <scott.abbey@homes.com>
2021-10-14 10:15:43 -0400

    fix completions

--- zsh/plugins_before.zsh
+++ zsh/plugins_before.zsh
@@ -16,3 +16,8 @@ path_prepend "$HOME/.fzf/bin"
 # Key bindings
 # ------------
 source "$HOME/.fzf/shell/key-bindings.zsh"
+
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+autoload -Uz compinit && compinit
