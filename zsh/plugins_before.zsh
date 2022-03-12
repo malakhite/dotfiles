e33ff10d11f67124a8bd8824bdba0e11aba2750d
Scott Abbey <scott@scottabbey.com>
2022-03-11 20:14:47 -0500

    fix keybinds for word jump

--- zsh/plugins_before.zsh
+++ zsh/plugins_before.zsh
@@ -25,4 +25,5 @@ autoload -Uz compinit && compinit
 bindkey  "^[[H"   beginning-of-line
 bindkey  "^[[F"   end-of-line
 bindkey  "^[[3~"  delete-char
-
+bindkey  "^[[1;3D" backward-word
+bindkey  "^[[1;3C" forward-word
