835fa025f6848229c515710f51f3ff4c0c8b80e2
Scott Abbey <scott.abbey@xtuple.com>
2022-07-13 09:01:29 -0400

    update history settings

--- shell/env.sh
+++ shell/env.sh
@@ -1,5 +1,10 @@
 # Opt out of Microsoft dotnet telemetry
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
-export HISTFILE=~/.zhistory
-export SAVEHIST=10000
-export HISTSIZE=1000
+
+# History settings
+export HISTFILESIZE=1000000000
+export HISTSIZE=1000000000
+export HISTFILE=~/.zsh_history
+
+setopt HIST_FIND_NO_DUPS
+setopt INC_APPEND_HISTORY
